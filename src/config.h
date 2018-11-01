9c919643a8d3d84c7079c99137b97753c7a97d75
Robert Marko <robimarko@gmail.com>
2018-11-01 17:50:08 +0100

    Update EEPROM I2C adress
    MeshBMS uses ST M24256 I2C EEPROM and we have configured it to use adress 0x57 since 0x50 is already taken.

--- src/config.h
+++ src/config.h
@@ -3,7 +3,7 @@
 #define LED_PORT_NAME B
 #define LED_PIN 0
 static constexpr uint8_t source_i2c_address_for_program =
-    0x50; // default address for CAT24M01 I2C EEPROM
+    0x57; // default address for ST M24256 I2C EEPROM
 
 static constexpr uint32_t eeprom_not_programmed =
     0xFFFFFFFF; // default value in eeprom when not programmed
